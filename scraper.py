0abf88b0382deb0a99fcefb8d04fbbac3598a8e5
MattWong-ca <matthew.yt.wong@gmail.com>
2025-04-19 13:02:07 +0800

    misc

--- scraper.py
+++ scraper.py
@@ -306,7 +306,7 @@ async def main():
     load_checkpoint()
 
     influencers_df = get_influencers()
-    influencers_df = influencers_df[2630:]
+    influencers_df = influencers_df[4967:]
     if influencers_df.empty:
         logger.error("No influencers found. Exiting.")
         return
